4b655e0e1be132805bb7ca45193d358a0a7978de
Joseph Noblett <noblettjoe@gmail.com>
2024-03-22 21:21:14 -0400

    /api/project endpoints complete

--- api/projects/projects-middleware.js
+++ api/projects/projects-middleware.js
@@ -13,14 +13,16 @@ const validate_Project_ID = async (req, res, next) => {
 }
 
 const validate_Project = (req, res, next) => {
-  const { name, description } = req.body;
-  if (typeof name === 'string' && typeof description === 'string' || name.trim() === '' || description.trim() === '') {
+  const { name, description, completed } = req.body;
+  if (typeof name !== 'string' || typeof description !== 'string' || typeof completed !== 'boolean' || name.trim() === '' || description.trim() === '') {
     res.status(400).json({
       message: 'name and description required'
     })
   } else {
-    req.name = name;
-    req.description = description;
+    req.name = name.trim();
+    req.description = description.trim();
+    req.completed = completed 
+
     next();
   }
 }
--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -24,5 +24,40 @@ router.get('/:id', validate_Project_ID, (req, res, next) => {
     .catch(next)
 })
 
+router.post('/', validate_Project, (req, res, next) => {
+  Project.insert({ name: req.name, description: req.description, completed: req.completed })
+    .then(new_project => {
+      res.json(new_project);
+      console.log(new_project)
+    })
+    .catch(next)
+})
+
+router.put('/:id', validate_Project_ID, validate_Project, (req, res, next) => {
+  Project.update(req.params.id, { name: req.name, description: req.description, completed: req.completed })
+    .then(updated_project => {
+      res.json(updated_project);
+    })
+    .catch(next)
+})
+
+router.delete('/:id', validate_Project_ID, async (req, res, next) => {
+  try {
+    await Project.remove(req.params.id);
+    const projects = await Project.get();
+    res.status(200).json(projects)
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.get('/:id/actions', validate_Project_ID, (req, res, next) => {
+  Project.getProjectActions(req.params.id)
+    .then(actions => {
+      res.json(actions);
+    })
+    .catch(next)
+})
+
 module.exports = router;
 
