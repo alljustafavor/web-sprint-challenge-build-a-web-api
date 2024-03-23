8e0a075ee2a914bdbdf09bc512d44cfa4280b5f2
Joseph Noblett <noblettjoe@gmail.com>
2024-03-22 20:06:21 -0400

    /api/project returns correct data

--- api/projects/projects-middleware.js
+++ api/projects/projects-middleware.js
@@ -1,1 +1,28 @@
-// add middlewares here related to projects
+const Project = require('./projects-model');
+
+const validate_Project_ID = async (req, res, next) => {
+  const project_ID = await Project.get(req.params.id);
+  if (!project_ID) {
+    res.status(404).json({
+      message: "id not found",
+    });
+  } else {
+    req.project_ID = project_ID;
+    next();
+  }
+}
+
+const validate_Project = (req, res, next) => {
+  const { name, description } = req.body;
+  if (typeof name === 'string' && typeof description === 'string' || name.trim() === '' || description.trim() === '') {
+    res.status(400).json({
+      message: 'name and description required'
+    })
+  } else {
+    req.name = name;
+    req.description = description;
+    next();
+  }
+}
+
+module.exports = { validate_Project_ID, validate_Project};
--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const Project = require('./projects-model');
 const Action = require('../actions/actions-model');
-/* const {   } = require('./projects-middleware'); */
-
-
+const { validate_Project_ID, validate_Project } = require('./projects-middleware'); 
 
 const router = express.Router();
+
+router.get('/', (req, res) => {
+  Project.get()
+    .then(projects => {
+      res.json(projects);
+    })
+    .catch(err => {
+      res.json('[]');
+    })
+  })
 
 module.exports = router;
 
